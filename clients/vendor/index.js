94653ad6da55e216b4f3fed21e6372adac673b81
Jeremy Cleland <jeremy@clelandco.com>
2023-02-28 22:13:00 -0500

    lab 12 complete

--- clients/vendor/index.js
+++ clients/vendor/index.js
@@ -1,10 +1,19 @@
 'use strict';
 
-const eventPool = require('../eventPool');
 const handler = require('./handler');
 
-eventPool.on('VENDOR', (store) => {
-  setTimeout(() => {
-    handler(store);
-  }, 2000);
+const { io } = require('socket.io-client');
+const socket = io.connect('http://localhost:3005/caps');
+
+let store = chance.companyName();
+
+socket.emit('join', store);
+
+setInterval(() => {
+  handler(store);
+}, 5000);
+
+socket.on('delivered', (payload) => {
+  console.log(`VENDOR: Thank you for delivering ${payload.orderID}`);
+  process.exit(0);
 });
